87f05c2bab8980f24bf6678aa993188578036faa
Tyler LeBlond <tylerleblond@gmail.com>
2023-03-20 14:47:50 -0400

    Finished BellPairInit.

--- include/lsqecc/patches/dense_slice.hpp
+++ include/lsqecc/patches/dense_slice.hpp
@@ -51,8 +51,8 @@ struct DenseSlice : public Slice
     // Return a cell of the placed patch
     // TRL 01/25/23: Added second argument to be consistent with 'bool distillation'
     Cell place_sparse_patch(const SparsePatch& sparse_patch, bool distillation);
-    // TRL 03/17/23: Added multi-patch cell functionality
-    void place_sparse_patch_multiple_patches(const SparsePatch& sparse_patch);
+    // TRL 03/17/23: Added multi-cell patch functionality
+    void place_sparse_patch_multiple_cells(const SparsePatch& sparse_patch);
 
     void delete_patch_by_id(PatchId id);
 
