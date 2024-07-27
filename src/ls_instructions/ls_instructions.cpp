85433e1e1310da93939c793afe3d8a31cb10e3d4
Tyler LeBlond <tylerleblond@gmail.com>
2024-07-27 10:21:42 -0400

    Fixed EDPC for even route lengths, enabled EDPC for condensed layout, and added tests.

--- src/ls_instructions/ls_instructions.cpp
+++ src/ls_instructions/ls_instructions.cpp
@@ -179,8 +179,8 @@ std::ostream& operator<<(std::ostream& os, const BellBasedCNOT& instruction)
                 if (i != instruction.counter_pairs.value()[phase].second - 1) 
                     os << ";";
             }
-            os << "]";
         }
+        os << "]";
     }
 
     return os;
